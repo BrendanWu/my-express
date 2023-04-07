db8c9a1fcb13f901befe5c0c1d84da7dff9100a4
shivam vora <vorashivam0@gmail.com>
2023-04-07 17:48:26 +0530

    return token

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/api/login', async (req, res) => {
     return res.status(400).json({ message: 'Invalid credentials' });
   }
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header('auth-token', token).json({ message: 'Logged in successfully', profile:{...user._doc, password:null} });
+  res.header('auth-token', token).json({ message: 'Logged in successfully', profile:{...user._doc, password:null,token:token} });
 });
 
 // Define signup API endpoint
